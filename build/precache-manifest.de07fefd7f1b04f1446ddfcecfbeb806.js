e1886782c09eebdafe3dbb76e3feabafbe858d78
Adrián Moreno <adrianmn90@gmail.com>
2019-04-05 18:12:48 +0200

    Initial commit

---
+++ build/precache-manifest.de07fefd7f1b04f1446ddfcecfbeb806.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eab043a382a42078ccaf792d95ee646f",
+    "url": "/static/media/logoredandwhite.eab043a3.png"
+  },
+  {
+    "revision": "391776efae76b37ea9ecbd2dcb343528",
+    "url": "/static/media/LogoInstaInvertido.391776ef.png"
+  },
+  {
+    "revision": "6974b5f9158cbe064b8e465e68e52eed",
+    "url": "/static/media/LogoInstaBN.6974b5f9.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf73f7ae9f2237ffcdeb",
+    "url": "/static/js/main.d9e0d695.chunk.js"
+  },
+  {
+    "revision": "5a105fabc3143ca1d5bc",
+    "url": "/static/js/2.5527cc1c.chunk.js"
+  },
+  {
+    "revision": "bf73f7ae9f2237ffcdeb",
+    "url": "/static/css/main.5667c5ae.chunk.css"
+  },
+  {
+    "revision": "5a105fabc3143ca1d5bc",
+    "url": "/static/css/2.c975d818.chunk.css"
+  },
+  {
+    "revision": "3a11cb855afdbd8d115c7589be07766d",
+    "url": "/index.html"
+  }
+];
