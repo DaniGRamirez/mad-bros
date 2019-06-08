0ae2886f684605539398e93c23cfdc9bfd784613
DANIEL <daniel@iMac-de-DANIEL.local>
2019-06-08 13:55:07 +0200

    Buidling

---
+++ build/precache-manifest.3a313e102832b75d5959eb60ab9b1f32.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eab043a382a42078ccaf792d95ee646f",
+    "url": "/static/media/logoredandwhite.eab043a3.png"
+  },
+  {
+    "revision": "61d52a68cfb7c00c8a30a73c1937ce0f",
+    "url": "/static/media/foto-8.61d52a68.jpg"
+  },
+  {
+    "revision": "391776efae76b37ea9ecbd2dcb343528",
+    "url": "/static/media/LogoInstaInvertido.391776ef.png"
+  },
+  {
+    "revision": "6974b5f9158cbe064b8e465e68e52eed",
+    "url": "/static/media/LogoInstaBN.6974b5f9.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "946f106807e57c53b9a2",
+    "url": "/static/js/main.7cba3915.chunk.js"
+  },
+  {
+    "revision": "6cc5fb9c3d880ad9c670",
+    "url": "/static/js/2.99314c38.chunk.js"
+  },
+  {
+    "revision": "946f106807e57c53b9a2",
+    "url": "/static/css/main.8d29a32e.chunk.css"
+  },
+  {
+    "revision": "6cc5fb9c3d880ad9c670",
+    "url": "/static/css/2.1f284f7e.chunk.css"
+  },
+  {
+    "revision": "5ff9db003aef1cc9cf8a15b5b3fcaa44",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58e3377b5d515650ed732e0f3765e103.js"
+  "/precache-manifest.3a313e102832b75d5959eb60ab9b1f32.js"
 );
 
 workbox.clientsClaim();
